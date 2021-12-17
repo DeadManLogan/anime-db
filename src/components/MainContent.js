11c87c4901b7854c0c80dc6c6177a96fca17fa75
DeadManLogan <grigoroudisdimosthenis@gmail.com>
2021-12-17 21:18:24 +0200

    Functionality completed

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import AnimeCard from './AnimeCard';
 
 function MainContent(props){
     return(
@@ -13,6 +14,13 @@ function MainContent(props){
                         value={props.search}
                         onChange={e => props.SetSearch(e.target.value)} />
                 </form>
+            </div>
+            <div className="anime-list">
+                {props.animeList.map(anime => (
+                    <AnimeCard 
+                    anime={anime}
+                    key={anime.mal_id} />
+                ))}
             </div>
         </main>
     )
